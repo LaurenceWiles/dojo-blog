61fcd34b3d95f42fd25e37facbbadf375cbce903
LaurenceWiles <140092148+LaurenceWiles@users.noreply.github.com>
2024-11-15 16:47:37 +0100

    refs vs rective

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,31 +1,33 @@
 <template>
   <div class="home">
-    <p ref="p">My name is {{ name }} and I am {{ age }} years old.</p>
-    <button @click="handleClick">Click Me</button>
-    <button @click="age++">Add 1 to age</button>
-    <input type="text" v-model="name" />
+    <h1>Home</h1>
+    <h2>Refs</h2>
+    <p>{{ ninjaOne.name }} - {{ ninjaOne.age }}</p>
+    <button @click="updateNinjaOne">updateNinjaOne</button>
+    <h2>Reactive</h2>
+    <p>{{ ninjaTwo.name }} - {{ ninjaTwo.age }}</p>
+    <button @click="updateNinjaTwo">updateNinjaTwo</button>
   </div>
 </template>
 
 <script>
-import { ref } from "vue";
+import { reactive, ref } from "vue";
 
 // @ is an alias to /src
 
 export default {
   name: "HomeView",
   setup() {
-    const p = ref(null);
+    const ninjaOne = ref({ name: "mario", age: 30 });
+    const ninjaTwo = reactive({ name: "luigi", age: 35 });
 
-    const name = ref("mario");
-    const age = ref(30);
-
-    const handleClick = () => {
-      name.value = "luigi";
-      age.value = 35;
+    const updateNinjaOne = () => {
+      ninjaOne.value.age = 40;
     };
-
-    return { name, age, handleClick, p };
+    const updateNinjaTwo = () => {
+      ninjaTwo.age = 45;
+    };
+    return { ninjaOne, updateNinjaOne, ninjaTwo, updateNinjaTwo };
   },
 };
 </script>
